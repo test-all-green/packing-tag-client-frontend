3289878742de7029a6e46905c042010c2b04ead2
AndrewLi <13058100289@163.com>
2019-07-28 22:31:31 +0800

    update host

--- src/api/login.js
+++ src/api/login.js
@@ -2,6 +2,6 @@ import {get, post, put, _delete} from '@/utils/http'
 
 export function login(data) {
   console.log("login...", data);
-  let url = 'http://localhost:8088/parking-staffs/login'
+  let url = 'http://39.98.244.95:8088/parking-staffs/login'
   return post(url, data);
 }
