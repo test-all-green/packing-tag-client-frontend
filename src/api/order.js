85797c9a59c3effba443f86f8f10f6b08ffda7cc
fanlun008 <549734423@qq.com>
2019-07-31 01:20:27 +0800

    fix: backend port

--- src/api/order.js
+++ src/api/order.js
@@ -1,14 +1,14 @@
 import {get, post, put, _delete} from '@/utils/http'
 
 export function getOrders(status){
-    return get('http://localhost:8088/parking-orders',{status:status})
+    return get('http://39.98.243.100:8088/parking-orders',{status:status})
 }
 export function pushOrder(data){
-    return post('http://localhost:8088/parking-orders',data);
+    return post('http://39.98.243.100:8088/parking-orders',data);
 }
 export function getHistoryOrder(){
-    return get('http://localhost:8088/parking-orders/history');
+    return get('http://39.98.243.100:8088/parking-orders/history');
 }
 export function putGrapOrder(data){
-    return put('http://localhost:8088/parking-orders/grabOrder',data)
+    return put('http://39.98.243.100:8088/parking-orders/grabOrder',data)
 }
