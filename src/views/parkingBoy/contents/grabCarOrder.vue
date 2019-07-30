c2bf8e34a5e278ab0784e6529bd55daf83e8289a
fanlun008 <549734423@qq.com>
2019-07-30 15:23:52 +0800

    fix: fix orderListPage

--- src/views/parkingBoy/contents/grabCarOrder.vue
+++ src/views/parkingBoy/contents/grabCarOrder.vue
@@ -13,9 +13,30 @@
     </van-cell>-->
     <!-- <mt-cell v-for="(item,index) in orderList" :key="index" :title="item.carNum"  value="带 icon"></mt-cell>   -->
 
-    <div class="box-card-group" v-for="order in this.orderList">
-      <el-card style="width: 100%;">
-        {{order.status}}
+    <div v-for="order in this.orderList" :key="order.id">
+      <el-card class="box-card" style="width: 100%;" shadow="hover">
+        <div class="card-body">
+          <el-row>
+            <el-col :span="4" :offset="1">
+              <!-- <span class="circle">
+                <p>订单</p>
+              </span>-->
+              <el-avatar :size="52" src="../../../assets/logo.png"></el-avatar>
+            </el-col>
+            <el-col :span="9" :offset="1">
+              <div class="order-content-mid">
+
+              </div>
+            </el-col>
+
+            <el-col :span="4" :offset="14">
+              <div class="right">
+                详情
+                <i class="el-icon-right"></i>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
       </el-card>
     </div>
   </div>
@@ -39,15 +60,51 @@ export default {
 };
 </script>
 
-<style>
+<style  lang='scss'>
 .grab-car-order-container {
   text-align: left;
+  height: 562px;
+  overflow: auto;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  .el-card__body {
+    padding: 0;
+    text-align: left;
+    height: 80px;
+    padding: 10px 0 0 0;
+  }
+  .card-body {
+    position: relative;
+  }
+  .circle {
+    display: inline-block;
+    height: 60px;
+    width: 60px;
+    border-radius: 50%;
+    background: #f3ada1;
+    line-height: 60px;
+    // margin-left: 20px;
+    & p {
+      // margin:0 auto;
+      font-size: 23px;
+      margin-left: 7px;
+    }
+  }
+  .right {
+    font-size: 16px;
+    line-height: 20px;
+    color: #a2a2a2;
+    margin-top: 22px;
+
+   
+  }
 }
 .grab-car-order-cell-value {
   display: inline-block;
   border: 1px solid black;
   border-radius: 50%;
-  padding: 5px;
+  padding: 1px;
   background-color: red;
 }
 </style>
