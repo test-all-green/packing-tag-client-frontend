8b4d6949c0150d22f7fc76e9d75b8f64a8af201a
fanlun008 <549734423@qq.com>
2019-07-31 16:52:05 +0800

    fix: show the order detail page

--- src/views/custom/contents/order.vue
+++ src/views/custom/contents/order.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="order-user-show">
-    <div class="back-to-list" v-if="this.isInDetailPage">
+    <div class="back-to-list" v-if="this.isInDetailPage" @click="backToList">
       <i class="el-icon-back"></i>
     </div>
     <div class="user-orderList-wrapper" v-if="!this.isInDetailPage">
@@ -18,7 +18,7 @@
                 <p class="status-text">状态：{{item.status|orderStatusFileter}}</p>
               </el-col>
               <el-col :span="4" :offset="8">
-                <div class="right">
+                <div class="right" @click="showDetailPage(item)">
                   详情
                   <i class="el-icon-right"></i>
                 </div>
@@ -30,8 +30,41 @@
     </div>
 
     <div class="user-order-detail-page" v-if="this.isInDetailPage">
-      <van-panel title="标题" desc="描述信息" status="状态">
-        <div>内容</div>
+      <van-panel
+        :title="this.orderItem.carNum"
+        :desc="this.orderItem.parkingCreateTime"
+        :status="this.orderItem.status"
+      >
+        <div class="detail-content-container">
+          <div class="detail-info">
+            <el-row>
+              <el-col :span="7" :offset="2">服务人: </el-col>
+              <el-col :span="10">{{this.orderItem.parkingBoyName}}</el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="7" :offset="2">联系电话：</el-col>
+              <el-col :span="10">{{this.orderItem.parkingBoyPhone}}</el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="7" :offset="2">交接点：</el-col>
+              <el-col :span="10">{{this.orderItem.parkingWaitLocation}}</el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="7" :offset="2">停车地点：</el-col>
+              <el-col :span="10">{{this.orderItem.parkingLocation}}</el-col>
+            </el-row>
+          </div>
+        </div>
+        <div slot="footer" class="detail-content-footer">
+          <el-row>
+            <el-col :span="10" :offset="1">
+              <van-button size="large">取车</van-button>
+            </el-col>
+            <el-col :span="10" :offset="1">
+              <van-button size="large" type="danger">支付订单</van-button>
+            </el-col>
+          </el-row>
+        </div>
       </van-panel>
     </div>
   </div>
@@ -43,8 +76,28 @@ import { getHistoryOrder } from "../../../api/order";
 export default {
   data() {
     return {
-      OrderData: [],
-      isInDetailPage: true
+      OrderData: [
+        {
+          id: 2,
+          carNum: "粤BGUN",
+          parkingCreateTime: "2019-11-11 10:24:11",
+          parkingEndTime: "1564413634943",
+          parkingBoyId: "12",
+          parkingBoyName: "康司机",
+          parkingBoyPhone: "1351002457",
+          parkingWaitLocation: "香洲区南软交界点",
+          parkingLotId: "1",
+          parkingLocation: "南软地下停车场",
+          createTime: "1564413634943",
+          endTime: "1564413634943",
+          status: "PI",
+          type: "0",
+          parkingLotType: "1",
+          regionId: "1"
+        }
+      ],
+      isInDetailPage: false,
+      orderItem: {}
     };
   },
 
@@ -62,6 +115,25 @@ export default {
     async initData() {
       const data = await getHistoryOrder();
       this.OrderData = data.data;
+    },
+
+    showDetailPage(order) {
+      this.isInDetailPage = true;
+      this.orderItem = order;
+      this.orderItem.status = this.orderStatusFileter(this.orderItem.status);
+    },
+    backToList() {
+      this.isInDetailPage = false;
+    },
+
+    orderStatusFileter(val) {
+      let map = {
+        PW: "无人受理",
+        PI: "停取中",
+        F: "已完成",
+        C: "取消订单"
+      };
+      return map[val];
     }
   },
 
@@ -87,6 +159,33 @@ export default {
   overflow: auto;
   &::-webkit-scrollbar {
     display: none;
+  }
+
+  .user-order-detail-page {
+    .van-panel__header {
+      .van-cell__title {
+        margin-left: 13px;
+        span {
+          font-size: 24px;
+          color: #9a9292;
+        }
+      }
+      .van-cell__value {
+        span {
+          font-size: 16px;
+          font-weight: 800;
+        }
+      }
+    }
+    .detail-content-container {
+      padding: 15px 0;
+      .detail-info {
+          color: #584f4f;
+          .el-row {
+              font: 14px/2 Tahoma,Helvetica,Arial,'宋体',sans-serif;
+          }
+      }
+    }
   }
   .back-to-list {
     position: absolute;
